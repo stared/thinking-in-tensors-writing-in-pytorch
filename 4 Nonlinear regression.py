e5293ce5025785d393b14562f67cd15ce614509e
Piotr Migdał <pmigdal@gmail.com>
2019-04-14 15:24:10 +0200

    testing jupytext

---
+++ 4 Nonlinear regression.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.3'
+#       jupytext_version: 1.0.5
+#   kernelspec:
+#     display_name: Python [default]
+#     language: python
+#     name: python3
+# ---
+
+# # Thinking in tensors, writing in PyTorch
+#
+# A hands-on course by [Piotr Migdał](https://p.migdal.pl) (2019).
+#
+# ## Notebook 4: Non-linear regression
+#
+# Some 
+#
+# ![](https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/Correlation_examples2.svg/400px-Correlation_examples2.svg.png)
+#
+#
+# ### Exercise
+#
+# Which of the following can be described by linear regression:
+#
+# * without any modifications,
+# * by after rescaling *x* or *y*,
+# * cannot be described by linear regression?
+#
+# **TODO**
+#
+# * Prepare examples
+# * 1d function with nonlinearities (by hand and automatically)
+# * More advanced
+
+
